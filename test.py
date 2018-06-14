fdba214e0e61393b6f95cda4c7f0f9b230addd57
Saul Shanabrook <s.shanabrook@gmail.com>
2018-06-14 07:59:23 -0400

    fixes

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import numba.plures.typing
 from numba import jit
 from xnd import xnd
 
+
 @jit(nopython=True)
 def hi(x):
-    print("has error", x.err_occurred)
-    print("type", x.type)
-    print("value", x)
     return x
 
-print(hi(xnd([10])))
+
+print(hi(xnd(10)))
+
